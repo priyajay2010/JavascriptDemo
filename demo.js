42bed496d2d79bca24df293b114bf399a83b2ca7
Priya Jay <priya.budsp@gmail.com>
2022-06-24 11:37:31 +0530

    Updated by removing command lines

--- demo.js
+++ demo.js
@@ -1,77 +1,77 @@
 //Finding Divisor and count of it
 
-// var number= prompt("Enter The Number")
-// var divisor=1
-// var count=0
-// while(divisor<=number)
-// {
-// 	if(number%divisor==0)
-// 	{
-// 	console.log(divisor + " is divisor of " + number)
-// 	count++;
-// 	//alert(divisor)
+var number= prompt("Enter The Number")
+var divisor=1
+var count=0
+while(divisor<=number)
+{
+	if(number%divisor==0)
+	{
+	console.log(divisor + " is divisor of " + number)
+	count++;
+	//alert(divisor)
 
-// }
-// 	divisor++;
-// }
-// console.log("Number of Divisor of "+  number + " is " + count)
+}
+	divisor++;
+}
+console.log("Number of Divisor of "+  number + " is " + count)
 
 
 
 //Finding divisible of 2
-// var number=100
-// var divisor=1
-// while(divisor<=number)
-// {
-// 	
-// 	if(divisor%2==0)
-// 	{
-// 		console.log(divisor + " is divisible of " + 2)
-// 	}
-// 	divisor++;
-// }
+var number=100
+var divisor=1
+while(divisor<=number)
+{
+	
+	if(divisor%2==0)
+	{
+		console.log(divisor + " is divisible of " + 2)
+	}
+	divisor++;
+}
 
 
 //Finding divisible of 3
-// var number=100
-// var divisor=1
-// while(divisor<=number)
-// {
+var number=100
+var divisor=1
+while(divisor<=number)
+{
 	
-// 	if(divisor%3==0)
-// 	{
-// 		console.log(divisor + " is divisible of 3")
-// 	}
-// 	divisor++;
-// }
+	if(divisor%3==0)
+	{
+		console.log(divisor + " is divisible of 3")
+	}
+	divisor++;
+}
 
 
 //Finding divisible of 2 & 3
-// var number=100
-// var divisor=1
-// while(divisor<=number)
-// {
+var number=100
+var divisor=1
+while(divisor<=number)
+{
 	
-// 	if(divisor%2==0  & divisor%3==0)
-// 	{
-// 		console.log(divisor + " is divisible of 2 & 3")
-// 	}
-// 	divisor++;
-// }
+	if(divisor%2==0  & divisor%3==0)
+	{
+		console.log(divisor + " is divisible of 2 & 3")
+	}
+	divisor++;
+}
 
 
  //Finding divisible of 2 || 3
-// var number=100
-// var divisor=1
-// while(divisor<=number)
-// {
+var number=100
+var divisor=1
+while(divisor<=number)
+{
 	
-// 	if(divisor%2==0  || divisor%3==0)
-// 	{
-// 		console.log(divisor + " is divisible of 2 or 3")
-// 	}
-// 	divisor++;
-// }
+	if(divisor%2==0  || divisor%3==0)
+	{
+		console.log(divisor + " is divisible of 2 or 3")
+	}
+	divisor++;
+}
 
 //Finding prime number
 
